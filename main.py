1f2059772af913c70c095362b358ab95c92c6405
Greg Hawthorne <ghawthorne@me.com>
2020-11-26 10:40:15 +0100

    search method

--- main.py
+++ main.py
@@ -59,6 +59,24 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------FIND PASSWORD ---------------------------#
+def find_password():
+    website = website_entry.get()
+    website = website.title()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Website: {website}\nEmail: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No Data File Found for {website}")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -79,8 +97,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -93,5 +111,7 @@ generate_password_button = Button(text="Generate Password", command=generate_pas
 generate_password_button.grid(row=3, column=2)
 add_button = Button(text="Add", width=35, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", padx=30, command=find_password)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
